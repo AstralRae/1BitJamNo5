014d3a08b7663dd735b46c8d8dde14a631a12bf3
Niv <162759280+nivproject@users.noreply.github.com>
2025-02-01 15:57:23 -0700

    Pause Menu Functionality

--- level_1.gd
+++ level_1.gd
@@ -3,15 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	# TODO: Play Level 1 music
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta) -> void:
 	pass
 	
 func _on_killzone_area_entered(area: Area2D):
+	# TODO: Play death noise
 	reset_level()
 	
 func reset_level():
 	$Player.position = $PlayerSpawn.position
+	# TODO: Reset wind/obstacles/enemies/etc.
--- player.gd
+++ player.gd
@@ -1,17 +1,27 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var gravity = 980
 
-func _physics_process(delta: float) -> void:
+func _ready():
+	## Load but hide the pause menu on scene load
+	$Camera2D/PauseMenu.hide()
+
+func _physics_process(delta: float):
+	#3 Display pause menu and pause the game
+	if Input.is_action_just_pressed("menu"):
+		get_tree().paused = true
+		$Camera2D/PauseMenu.show()
+		$Camera2D/PauseMenu/ResumeButton.grab_focus()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and $UnpauseTimer.is_stopped():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -21,5 +31,34 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# Handle vine mechanic
+	if Input.is_action_pressed("vine"):
+		velocity.x = velocity.x/2
+		# TODO: Prevent wind from interacting with player
+		# TODO: Play vine animation
 
 	move_and_slide()
+
+
+func _on_pause_menu_resume_signal():
+	## Unpause the game and set a timer to prevent the unpause button from jumping
+	get_tree().paused = false
+	$Camera2D/PauseMenu.release_focus()
+	$Camera2D/PauseMenu.hide()
+	$UnpauseTimer.start()
+
+func _on_pause_menu_quit_signal():
+	## Unpause the game in case the player reloads it from main menu
+	get_tree().paused = false
+	$Camera2D/PauseMenu.release_focus()
+	$Camera2D/PauseMenu.hide()
+	$UnpauseTimer.start()
+	
+	## Switch back to main menu
+	var old_level = get_tree().current_scene # Prepare to unload prior level
+	old_level.queue_free() # Unload prior level 
+	
+	var level_instance = load("res://main.tscn").instantiate() # Load & instantiate main menu
+	get_tree().root.add_child(level_instance) # Add main menu as a child of the root tree
+	get_tree().set_current_scene.call_deferred(level_instance) # Set main menu as the new scene
