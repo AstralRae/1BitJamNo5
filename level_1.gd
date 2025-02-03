381b292231deb5c7a7f34aa29386e86b7896169e
Niv <162759280+nivproject@users.noreply.github.com>
2025-02-03 10:41:16 -0700

    Popup Code

--- level_1.gd
+++ level_1.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Player.publicReset.call()
 	pass
 	# TODO: Play Level 1 music
 
@@ -16,9 +17,18 @@ func _on_killzone_area_entered(area: Area2D):
 	
 func reset_level():
 	$Player.position = $PlayerSpawn.position
+	$Player.publicReset.call()
 	# TODO: Reset wind/obstacles/enemies/etc.
 
 func _on_wind_area_entered(area: Area2D, new_wind_speed: float, new_wind_angle: float) -> void:
 	$Player.WIND_SPEED = new_wind_speed
 	$Player.WIND_ANGLE = new_wind_angle
 	return
+
+func _on_first_popup_area_entered(area: Area2D):
+	$FirstPopup/FirstPopupLabel.show()
+	# TODO: Add popup noise
+
+func _on_first_popup_area_exited(area: Area2D):
+	$FirstPopup/FirstPopupLabel.hide()
+	
